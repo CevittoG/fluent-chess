2a43efe216cafae27ae22aa904acae47639e9acf
Seba Gutierrez <sebagm123@gmail.com>
2024-03-01 23:00:38 -0600

    Pawn special move (en passant)

--- src/board.py
+++ src/board.py
@@ -47,7 +47,7 @@ class Board:
             cute_print(f"{piece.color}_{piece.type} at {start_position} can't move to {end_position}", f'{piece.color}_{piece.type}', 'yellow')
         elif piece is not None and end_position in piece_valid_moves:
             # Update the piece state
-            piece.has_moved = True
+            piece.movements.append(end_position)
             piece.current_square = end_position
             # Update the board state
             self.board[start_position[0]][start_position[1]] = None
@@ -55,23 +55,6 @@ class Board:
 
             cute_print(f"{piece.color}_{piece.type}: {start_position} -> {end_position}", f'{piece.color}_{piece.type}')
 
-    # def is_valid_move(self, piece, start_position, end_position):
-    #     """
-    #     Placeholder method to check if a move is valid (not implemented yet).
-    #
-    #     Args:
-    #         piece: The piece object being moved.
-    #         start_position: A tuple (row, col) representing the starting position.
-    #         end_position: A tuple (row, col) representing the desired ending position.
-    #
-    #     Returns:
-    #         True if the move is valid, False otherwise (currently always returns False).
-    #     """
-    #     return True  # Placeholder until actual validation is implemented
 
-    # def update_board(self, new_square, piece: Piece, captured_piece=None):
-    #     # Update board state after a valid move
-    #     self.board[new_square[0]][new_square[1]] = piece
-    #     # Implement logic for captured piece handling (if any)
-    #     # (update captured piece position or remove it from the board)
+
 
