1cfaeaa3c72510398d5eb1660575bc370671f9a2
Seba Gutierrez <sebagm123@gmail.com>
2024-03-13 11:09:59 -0500

    special move side

--- src/board.py
+++ src/board.py
@@ -76,13 +76,13 @@ class Board:
                 elif isinstance(piece, Pawn) and 'passant' in move_label:
                     self.perform_standard_move(piece, start_position, end_position)
                     self.perform_en_passant(piece, start_position, move_label)
-                    special_move = {'Type': 'En Passant', 'Obs': ''}  # ToDo: fix side
+                    special_move = {'Type': 'En Passant', 'Obs': move_label[move_label.index('-')+1:move_label.index('_')].title()}
                     capture_piece = 'Pawn'
                 # King castling
                 elif isinstance(piece, King) and 'castling' in move_label:
                     self.perform_castling(end_position, move_label)
                     self.perform_standard_move(piece, start_position, end_position)
-                    special_move = {'Type': 'Castling', 'Obs': 'Kingside'}  # ToDo: fix side
+                    special_move = {'Type': 'Castling', 'Obs': move_label[move_label.index('-')+1:move_label.index('_')].title()}
                 # Standard move
                 else:
                     self.perform_standard_move(piece, start_position, end_position)
